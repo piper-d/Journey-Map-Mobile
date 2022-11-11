00a5d447ceadf1bba445e5c5cba17bed1394f354
RyanS195 <ryanfsteinmetz@gmail.com>
2022-11-10 22:38:23 -0500

    Added Navigation

---
+++ JourneyMapApp/src/views/Archive/index.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text, SafeAreaView, Button } from "react-native";
+import { Props } from "../../../App";
+
+export function ArchiveView({route, navigation}: Props) {
+    return (
+      <SafeAreaView style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+        <Text>Details Screen</Text>
+      </SafeAreaView>
+    );
+  }
+
