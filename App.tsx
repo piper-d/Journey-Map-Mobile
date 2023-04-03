2d083fcac264fdd0bda8b030cb503cf25788e461
RyanS195 <ryanfsteinmetz@gmail.com>
2023-04-02 20:51:59 -0400

    refresh token

--- App.tsx
+++ App.tsx
@@ -2,8 +2,11 @@ import React from 'react';
 import { Routes } from './src/routes';
 import { NavigationContainer } from '@react-navigation/native';
 import { registerRootComponent } from 'expo';
+import { refreshToken } from './config/firebase';
 
 function App() {
+  // User stays signed in
+  refreshToken();
   return (
     <NavigationContainer>
       <Routes />
--- config/firebase.ts
+++ config/firebase.ts
@@ -4,6 +4,7 @@ import { getFirestore } from 'firebase/firestore';
 import { getFunctions } from 'firebase/functions';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { initializeAuth, getReactNativePersistence } from 'firebase/auth/react-native';
+import { useEffect } from 'react';
 
 //Initialize Firebase
 const firebaseConfig = {
@@ -23,4 +24,33 @@ const db = getFirestore(app);
 const functions = getFunctions(app);
 functions;
 
-export { auth, db, functions };
+// Add local storage token add and delete
+const refreshToken = () => {
+  useEffect(() => {
+    function refreshCurrentToken() {
+      const user = auth.currentUser;
+
+      if (user) {
+        user
+          .getIdToken(true)
+          .then((token) => {
+            AsyncStorage.setItem('accessToken', token);
+            console.log('Set Token');
+          })
+          .catch((error: any) => {
+            console.error('Error refreshing token:', error);
+          });
+      }
+    }
+
+    const tokenRefreshInterval = 57 * 60 * 1000; // Refresh every 57 minutes
+    const intervalId = setInterval(refreshCurrentToken, tokenRefreshInterval);
+
+    // Clean up the interval when the component is unmounted
+    return () => {
+      clearInterval(intervalId);
+    };
+  }, []);
+};
+
+export { auth, db, functions, refreshToken };
